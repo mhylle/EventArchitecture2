6a1721d892002812cac10d5e502b4c0266afac6d
mnh <martin.hylleberg@systematic.com>
2017-02-14 11:20:45 +0100

    rule engine can now create and associate a episodeof careelement to a referral request if no eoce's already exist (if there is even 1 it wont happen)

--- src/main/java/info/mhylle/playground/lpr3/Lpr3Application.java
+++ src/main/java/info/mhylle/playground/lpr3/Lpr3Application.java
@@ -1,14 +1,13 @@
 package info.mhylle.playground.lpr3;
 
 import info.mhylle.playground.lpr3.data.Repository;
-import info.mhylle.playground.lpr3.model.Encounter;
-import info.mhylle.playground.lpr3.model.EpisodeOfCareElement;
-import info.mhylle.playground.lpr3.model.Patient;
-import info.mhylle.playground.lpr3.model.Referral;
+import info.mhylle.playground.lpr3.model.*;
+import info.mhylle.playground.lpr3.model.SKS.episodeofcare.StatusCode;
 import info.mhylle.playground.lpr3.rules.Rule;
 import info.mhylle.playground.lpr3.rules.RuleEngine;
 
 import javax.ws.rs.core.Application;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,6 +15,7 @@ public class Lpr3Application extends Application
 {
   public Lpr3Application()
   {
+    System.out.println("Lpr3Application.Lpr3Application");
     RuleEngine.getInstance().addRule(new Rule()
     {
       @Override public boolean handle()
@@ -35,33 +35,37 @@ public class Lpr3Application extends Application
 
       @Override public boolean process(Object o)
       {
+        System.out.println("Lpr3Application.process");
         Referral referral = (Referral) o;
-        UUID encounter = referral.getEncounter();
-        if (encounter != null) {
-          Encounter e = Repository.getInstance().getEncounters().stream().filter(r -> r.getId().equals(encounter))
+        UUID pid = referral.getPatient();
+
+        if (pid != null) {
+          Patient p = Repository.getInstance().getPatients().stream().filter(r -> r.getId().equals(pid))
             .findFirst().orElse(null);
-          if (e != null) {
-            UUID patient = e.getPatient();
-            if (patient != null) {
-              Patient p = Repository.getInstance().getPatients().stream().filter(r -> r.getId().equals(patient))
-                .findFirst().orElse(null);
-              if (p != null) {
-                List<UUID> episodesOfCareElements = p.getEpisodesOfCareElements();
-                for (UUID episodesOfCareElement : episodesOfCareElements) {
-                  EpisodeOfCareElement eoce = Repository.getInstance().getEpisodeOfCareElements().stream().filter(r -> r.getId().equals(episodesOfCareElement))
-                    .findAny().orElse(null);
-                  if (eoce != null) {
-                    if (eoce.getReferral() != null) {
-                      if (eoce.getReferral().equals(referral.getId())) {
-                        return true;
-                      }
+          if (p != null) {
+            List<UUID> episodesOfCareElements = p.getEpisodesOfCareElements();
+            if (!episodesOfCareElements.isEmpty()) {
+              for (UUID episodesOfCareElement : episodesOfCareElements) {
+                EpisodeOfCareElement eoce = Repository.getInstance().getEpisodeOfCareElements().stream().filter(r -> r.getId().equals(episodesOfCareElement))
+                  .findAny().orElse(null);
+                if (eoce != null) {
+                  if (eoce.getReferral() != null) {
+                    if (eoce.getReferral().equals(referral.getId())) {
+                      // referral was already associated to an episode of care element, do nothing
+                      return true;
                     }
                   }
                 }
-                for (UUID episodesOfCareElement : episodesOfCareElements) {
-                  EpisodeOfCareElement eoce = Repository.getInstance().getEpisodeOfCareElements().stream().filter(r -> r.getId().equals(episodesOfCareElement))
-                    .findAny().orElse(null);
-                  if (eoce != null) {
+              }
+
+              for (UUID episodesOfCareElement : episodesOfCareElements) {
+                EpisodeOfCareElement eoce = Repository.getInstance().getEpisodeOfCareElements().stream().filter(r -> r.getId().equals(episodesOfCareElement))
+                  .findAny().orElse(null);
+                if (eoce != null) {
+                  if (referral.getEncounter() != null) {
+                    Encounter e = Repository.getInstance().getEncounters().stream()
+                      .filter(r -> r.getId().equals(referral.getEncounter()))
+                      .findAny().orElse(null);
                     if (eoce.getPeriod().getStartTime().isEqual(e.getPeriod().getStartTime())
                       || eoce.getPeriod().getStartTime().isBefore(e.getPeriod().getStartTime())) {
                       if (eoce.getPeriod().getEndTime().isEqual(e.getPeriod().getEndTime())
@@ -75,11 +79,51 @@ public class Lpr3Application extends Application
                   }
                 }
               }
+            } else {
+              createEpisodeOfCareElement(referral, p);
             }
           }
         }
         return false;
       }
     });
+  }
+
+  private void createEpisodeOfCareElement(Referral referral, Patient p)
+  {
+    // there are no episode of care elements, create one for this referral.
+    EpisodeOfCareElement eoce = new EpisodeOfCareElement();
+    eoce.setReferral(referral.getId());
+    eoce.setPatient(referral.getPatient());
+    eoce.setStatus(StatusCode.ACTIVE);
+    Period period = new Period();
+    period.setStartTime(LocalDateTime.now());
+    if (referral.getEncounter() != null) {
+      Encounter e = Repository.getInstance().getEncounters().stream()
+        .filter(r -> r.getId().equals(referral.getEncounter()))
+        .findAny().orElse(null);
+      if (e != null) {
+        period.setStartTime(e.getPeriod().getStartTime());
+        switch (e.getStatus()) {
+          case ARRIVED:
+            eoce.setStatus(StatusCode.ACTIVE);
+            break;
+          case PLANNED:
+            eoce.setStatus(StatusCode.PLANNED);
+            break;
+          case INPROGRESS:
+            eoce.setStatus(StatusCode.ACTIVE);
+          case ONLEAVE:
+            eoce.setStatus(StatusCode.ONHOLD);
+            break;
+          default:
+            eoce.setStatus(StatusCode.ACTIVE);
+        }
+      }
+    }
+    eoce.setPeriod(period);
+    p.addEpisodeOfCareElement(eoce.getId());
+    Repository.getInstance().addEpisodeOfCareElement(eoce);
+    Repository.getInstance().updatePatient(p);
   }
 }
